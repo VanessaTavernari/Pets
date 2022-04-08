8ada0ce3833255ed8a4e601e41147ee0454b1b88
Vanessa Tavares Tavernari <vanessa.tavernari@gmail.com>
2022-04-08 10:04:32 +0100

    Adds project running

--- pets/ViewController.swift
+++ pets/ViewController.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+extension UIColor {
+    static var mainPink = UIColor(red: 232/255, green: 68/255, blue: 68/255, alpha: 1)
+}
+
 class ViewController: UIViewController {
 
     //let´s avoid polluting viewDidLoad
@@ -18,25 +22,50 @@ class ViewController: UIViewController {
     let descriptionTextView: UITextView = {
         let textView = UITextView()
         
-        
         let attributedText = NSMutableAttributedString(string: "Dog", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)])
         
         attributedText.append(NSAttributedString(string: "\n\n\nAlong with their noses, their hearing is super sensitive", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 15), NSAttributedString.Key.foregroundColor: UIColor.gray]))
         
-        attributedText.append(NSAttributedString(string: "\n\n\nTheir sense of smell is at least 40x better than ours", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 15), NSAttributedString.Key.foregroundColor: UIColor.gray]))
+//        attributedText.append(NSAttributedString(string: "\n\n\nTheir sense of smell is at least 40x better than ours", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 15), NSAttributedString.Key.foregroundColor: UIColor.gray]))
 
         textView.attributedText = attributedText
 
-
-//        textView.text = "Their sense of smell is at least 40x better than ours"
-//        textView.font = UIFont.boldSystemFont(ofSize: 18)
-        textView.textAlignment = .justified
+        textView.textAlignment = .center
         textView.isEditable = false
         textView.isScrollEnabled = false
         textView.translatesAutoresizingMaskIntoConstraints = false
         return textView
-        
-        
+    }()
+    
+    
+    let previousButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("PREV", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.gray, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("NEXT", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        //let pinkColor = UIColor(red: 232/255, green: 68/255, blue: 133/255, alpha: 1)
+        button.setTitleColor(.mainPink, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let pageControl: UIPageControl = {
+        let pc = UIPageControl()
+        pc.currentPage = 0
+        pc.numberOfPages = 4
+        //let pinkColor = UIColor(red: 232/255, green: 68/255, blue: 133/255, alpha: 1)
+        pc.currentPageIndicatorTintColor = .mainPink
+        //pc.pageIndicatorTintColor = .gray
+        pc.pageIndicatorTintColor = UIColor(red: 249/255, green: 207/255, blue: 224/255, alpha: 1)
+        return pc
     }()
     
     override func viewDidLoad() {
@@ -46,10 +75,10 @@ class ViewController: UIViewController {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
         
-        //view.addSubview(imageView)
         view.addSubview(descriptionTextView)
-        setupLayout()
         
+        setupLayout()
+        setupBottomControls()
     }
     
     func setupLayout() {
@@ -76,7 +105,31 @@ class ViewController: UIViewController {
         descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor,constant: -20).isActive = true
         descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
     }
-
-
+    
+    func setupBottomControls() {
+//        view.addSubview(previousButton)
+//        previousButton.backgroundColor = .red
+        
+//        let yellowView = UIView()
+//        yellowView.backgroundColor = .yellow
+        
+//        let blueView = UIView()
+//        blueView.backgroundColor = .blue
+        
+//        let greenView = UIView()
+//        greenView.backgroundColor = .green
+        
+        let bottomControlsStackView = UIStackView(arrangedSubviews: [previousButton, pageControl, nextButton])
+        
+        bottomControlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomControlsStackView.distribution = .fillEqually
+        view.addSubview(bottomControlsStackView)
+        
+        
+        bottomControlsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        bottomControlsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        bottomControlsStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        bottomControlsStackView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
 }
 
